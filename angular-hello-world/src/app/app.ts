d951e0ab1ff2ad8c17765339ac8401c2607d77a5
Shriharsh <shriharshdpattar@gmail.com>
2026-04-21 14:16:20 +0530

    feat: update app component to use inline template and modify title message

--- angular-hello-world/src/app/app.ts
+++ angular-hello-world/src/app/app.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
+  //templateUrl: './app.html',
+  template: `<h1>{{ title() }}</h1>`, //Inline template
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Hello World');
+  protected readonly title = signal('Hello World from Angular 24!');
   name = "Shriharsh"
   age = 24
 
