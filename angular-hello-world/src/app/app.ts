774e5970819dcaa89867e7ab394fe573c398512e
Shriharsh <shriharshdpattar@gmail.com>
2026-04-22 18:42:20 +0530

    custom component created manually

--- angular-hello-world/src/app/app.ts
+++ angular-hello-world/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './login/login';
 import { Signup } from './signup/signup';
+import { ProfileComp } from './profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [Login, Signup],
+  imports: [Login, Signup, ProfileComp],
   templateUrl: './app.html',
   //template: `<h1>{{ title() }}</h1>`, //Inline template
   styleUrl: './app.css'
---
+++ angular-hello-world/src/app/profile/profile.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-profile',
+    template: `<h1>Profile works!</h1>`,
+    styles: `h1 { color: green; }`
+    //templateUrl: './profile.html',
+    //styleUrls: ['./profile.css']
+})
+
+export class ProfileComp {
+
+}
