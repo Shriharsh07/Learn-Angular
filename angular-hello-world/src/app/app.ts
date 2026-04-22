5028456c0e236e449f0fea956811ea3bb0306172
Shriharsh <shriharshdpattar@gmail.com>
2026-04-22 18:32:08 +0530

    feat: implement login and signup components with associated services and tests

--- angular-hello-world/src/app/app.ts
+++ angular-hello-world/src/app/app.ts
@@ -1,19 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  //templateUrl: './app.html',
-  template: `<h1>{{ title() }}</h1>`, //Inline template
+  imports: [Login, Signup],
+  templateUrl: './app.html',
+  //template: `<h1>{{ title() }}</h1>`, //Inline template
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('Hello World from Angular 24!');
   name = "Shriharsh"
   age = 24
-
-  updateTitle() {
-    this.name = "Shriharsh Pattar"
-  }
 }
